6a6054f4ad4033e5ff858082bcc6df21ae9f8bde
Kasia Meyer <kasia.empathy@outlook.com>
2024-10-25 01:10:31 +0300

    Move hostlists to its own folder

--- preset-youtube.bat
+++ preset-youtube.bat
@@ -10,6 +10,6 @@ set BIN=%~dp0bin\
 :: Define arguments for the winws.exe tool
 start "zapret: youtube" /min "%BIN%winws.exe" ^
 --wf-tcp=80,443 --wf-udp=443,50000-50100 ^
---filter-udp=443 --hostlist="list-youtube_quic.txt" --dpi-desync=fake --dpi-desync-repeats=2 --dpi-desync-cutoff=n2 --dpi-desync-fake-quic="%BIN%quic_pl_by_ori.bin" --new ^
---filter-tcp=443 --hostlist="list-youtube_gv.txt" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=10 --dpi-desync-ttl=4 --new ^
---filter-tcp=443 --hostlist="list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-split-seqovl=2 --dpi-desync-split-pos=2 --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" --dpi-desync-ttl=3
+--filter-udp=443 --hostlist="hostlists\list-youtube_quic.txt" --dpi-desync=fake --dpi-desync-repeats=2 --dpi-desync-cutoff=n2 --dpi-desync-fake-quic="%BIN%quic_pl_by_ori.bin" --new ^
+--filter-tcp=443 --hostlist="hostlists\list-youtube_gv.txt" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=10 --dpi-desync-ttl=4 --new ^
+--filter-tcp=443 --hostlist="hostlists\list-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-split-seqovl=2 --dpi-desync-split-pos=2 --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" --dpi-desync-ttl=3
--- update-blacklist_file.cmd
+++ update-blacklist_file.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 PUSHD "%~dp0"
-bitsadmin /transfer blacklist https://p.thenewone.lol/domains-export.txt "%CD%\list-russia_blacklist.txt"
+bitsadmin /transfer blacklist https://p.thenewone.lol/domains-export.txt "%CD%\hostlists\list-russia_blacklist.txt"
 POPD
