7c9592d542812df4a39236cc88a0f7c99d21167e
Kasia Meyer <kasia.empathy@outlook.com>
2024-10-26 04:56:14 +0300

    Add echo on successful service stop and delete

--- service-remove.cmd
+++ service-remove.cmd
@@ -13,46 +13,52 @@ if %errorlevel% neq 0 (
 :: Set the service name
 set SRVCNAME=zapret
 
-:: Stop the service if it is running
+:: Stop and delete zapret service
 echo Stopping service "%SRVCNAME%"...
 net stop "%SRVCNAME%" >nul 2>&1
 if errorlevel 1 (
     echo Service "%SRVCNAME%" is not running or does not exist.
+) else (
+    echo Service "%SRVCNAME%" stopped successfully.
 )
-
-:: Delete the service
 echo Deleting service "%SRVCNAME%"...
 sc delete "%SRVCNAME%" >nul 2>&1
 if errorlevel 1 (
     echo Failed to delete service "%SRVCNAME%". It may not exist.
+) else (
+    echo Service "%SRVCNAME%" deleted successfully.
 )
 
-:: Stop WinDivert if it is running
-echo Stopping service "%SRVCNAME%"...
+:: Stop and delete WinDivert service
+echo Stopping service "WinDivert"...
 net stop "WinDivert" >nul 2>&1
 if errorlevel 1 (
-    echo Service "%SRVCNAME%" is not running or does not exist.
+    echo Service "WinDivert" is not running or does not exist.
+) else (
+    echo Service "WinDivert" stopped successfully.
 )
-
-:: Delete WinDivert
-echo Deleting service "%SRVCNAME%"...
+echo Deleting service "WinDivert"...
 sc delete "WinDivert" >nul 2>&1
 if errorlevel 1 (
-    echo Failed to delete service "%SRVCNAME%". It may not exist.
+    echo Failed to delete service "WinDivert". It may not exist.
+) else (
+    echo Service "WinDivert" deleted successfully.
 )
 
-:: Stop WinDivert14 if it is running
-echo Stopping service "%SRVCNAME%"...
+:: Stop and delete WinDivert14 service
+echo Stopping service "WinDivert14"...
 net stop "WinDivert14" >nul 2>&1
 if errorlevel 1 (
-    echo Service "%SRVCNAME%" is not running or does not exist.
+    echo Service "WinDivert14" is not running or does not exist.
+) else (
+    echo Service "WinDivert14" stopped successfully.
 )
-
-:: Delete WinDivert14
-echo Deleting service "%SRVCNAME%"...
+echo Deleting service "WinDivert14"...
 sc delete "WinDivert14" >nul 2>&1
 if errorlevel 1 (
-    echo Failed to delete service "%SRVCNAME%". It may not exist.
+    echo Failed to delete service "WinDivert14". It may not exist.
+) else (
+    echo Service "WinDivert14" deleted successfully.
 )
 
 pause
